57566893dd89b3f07b54b14709b91e43aa4c2745
Tyler Brandt <tbrandtsiue@gmail.com>
2024-01-05 15:10:11 -0600

    Created index.html file to have buttons for backlight up, down, and info

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,42 @@
-import { createSocket } from 'dgram';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 import axios from 'axios';
-
-const sendSearchRequest = async () => {
-  return new Promise((resolve, reject) => {
-    const MULTICAST_ADDRESS = '239.255.255.250';
-    const MULTICAST_PORT = 1900;
-
-    const socket = createSocket('udp4');
-
-    const socketTimeout = setTimeout(() => {
-      socket.close();
-      console.log('Stopped listening for Roku devices');
-    }, 10000);
-
-    socket.on('message', (message, rinfo) => {
-      const response = message.toString();
-      if (response.includes('Roku')) {
-        console.log(`Roku found at ${rinfo.address}:${rinfo.port}`);
-        console.log('Response:\n', response);
-        socket.close();
-        clearTimeout(socketTimeout);
-
-        const locationMatch = response.match(/LOCATION: (.*)/i);
-        if (!locationMatch || !locationMatch[1])
-          reject('Could not parse LOCATION from roku device response');
-
-        resolve(locationMatch[1].trim());
-      }
-    });
-
-    socket.on('error', err => {
-      console.error('Socket error:', err);
-      reject(err);
-    });
-
-    socket.on('listening', () => {
-      socket.addMembership(MULTICAST_ADDRESS);
-      console.log('Socket is listening');
-    });
-
-    socket.bind(MULTICAST_PORT);
-
-    const message = Buffer.from(
-      'M-SEARCH * HTTP/1.1\r\n' +
-        `HOST: ${MULTICAST_ADDRESS}:${MULTICAST_PORT}\r\n` +
-        'MAN: "ssdp:discover"\r\n' +
-        'ST: roku:ecp\r\n' +
-        'MX: 3\r\n' +
-        '\r\n',
-    );
-
-    socket.send(message, 0, message.length, MULTICAST_PORT, MULTICAST_ADDRESS, err => {
-      if (err) {
-        console.error('Error sending M-SEARCH request:', err);
-      } else {
-        console.log('M-SEARCH request sent');
-      }
-    });
-  });
-};
-
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+import xml2js from 'xml2js';
+import { Commands, useKeySender } from './keys';
+import { CheckRokuAddress, GetRokuAddress } from './network';
 
 const main = async () => {
-  const address = await sendSearchRequest();
+  let address = await GetRokuAddress();
 
-  console.log({ address });
+  const server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    var isAddressGood = await CheckRokuAddress(address);
+    if (!isAddressGood) {
+      address = await GetRokuAddress();
+    }
 
-  const { data: deviceInfo } = await axios.get(`${address}/query/device-info`);
-  console.log({ deviceInfo });
+    var { sendKeySequence } = useKeySender(address);
 
-  const keypress = async (key: string, delay?: number) => {
-    await axios.post(`${address}keypress/${key}`);
-    await sleep(delay ?? 500);
-  };
-  const info = async (delay?: number) => await keypress('info', delay);
-  const up = async (delay?: number) => await keypress('up', delay);
-  const down = async (delay?: number) => await keypress('down', delay);
-  const left = async (delay?: number) => await keypress('left', delay);
-  const right = async (delay?: number) => await keypress('right', delay);
+    if (req.url === '/backlightDown') {
+      sendKeySequence(Commands.backlightDown);
+    } else if (req.url === '/backlightUp') {
+      sendKeySequence(Commands.backlightUp);
+    } else if (req.url === '/info') {
+      const { data: deviceInfo } = await axios.get(`${address}/query/device-info`);
+      const info = await xml2js.parseStringPromise(deviceInfo, {
+        explicitArray: false,
+        trim: true,
+      });
+      console.log({ info });
+    } else {
+      console.log('Unknown command:', req.url);
+    }
 
-  const backlightDown = async () => {
-    await info();
-    await down();
-    await right();
-    await down();
-    await down();
-    await right();
-    await down();
-    await down();
-    await right();
-    for (let i = 0; i < 100; i++) await left(5);
-    await info();
-  };
+    res.end();
+  });
 
-  const backlightUp = async () => {
-    await info();
-    await down();
-    await right();
-    await down();
-    await down();
-    await right();
-    await down();
-    await down();
-    await right();
-    for (let i = 0; i < 65; i++) await right(5);
-    await info();
-  };
-
-  const command = process.argv[2] ?? 'down';
-
-  switch (command) {
-    case 'down':
-      await backlightDown();
-      break;
-    case 'up':
-      await backlightUp();
-      break;
-    default:
-      await backlightDown();
-      break;
-  }
+  // Static LAN IP address assigned to tyler's 2020 MBP at 192.168.1.11
+  server.listen(3001, '0.0.0.0', () => {
+    console.log('Server running at http://192.168.1.11:3001/');
+  });
 };
 
 main();
---
+++ src/keys.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { sleep } from './utils';
+
+export type KEYS = 'up' | 'down' | 'left' | 'right' | 'info';
+
+export type KeyInput =
+  | KEYS
+  | { key: KEYS; delay: number }
+  | { key: KEYS; delay: number; repeat: number };
+
+export const useKeySender = (address: string) => {
+  const sendKeyPress = async (key: string, delay?: number) => {
+    await axios.post(`${address}keypress/${key}`);
+    await sleep(delay ?? 500);
+  };
+
+  return {
+    sendKeySequence: async (keypresses: KeyInput[]) => {
+      for (const keypress of keypresses) {
+        if (typeof keypress === 'string') {
+          await sendKeyPress(keypress);
+        } else if ('delay' in keypress && 'repeat' in keypress) {
+          for (let i = 0; i < keypress.repeat; i++) {
+            await sendKeyPress(keypress.key, keypress.delay);
+          }
+        } else if ('delay' in keypress) {
+          await sendKeyPress(keypress.key, keypress.delay);
+        }
+      }
+    },
+  };
+};
+
+export const Commands: {
+  backlightDown: KeyInput[];
+  backlightUp: KeyInput[];
+  [key: string]: KeyInput[];
+} = {
+  backlightDown: [
+    'info',
+    'down',
+    'right',
+    'down',
+    'down',
+    'right',
+    { key: 'left', repeat: 100, delay: 0 },
+    'info',
+  ],
+  backlightUp: [
+    'info',
+    'down',
+    'right',
+    'down',
+    'down',
+    'right',
+    { key: 'right', repeat: 65, delay: 0 },
+    'info',
+  ],
+};
