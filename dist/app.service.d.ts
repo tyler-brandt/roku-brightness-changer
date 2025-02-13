a4465b5c21d0051cac9a80b71518d583989b98bf
Tyler Brandt <tbrandtsiue@gmail.com>
2025-02-13 09:30:56 -0600

    ignore dist

--- dist/app.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { KeyInput } from 'src/network/keys';
-export declare class AppService {
-    sendKeySequence(commands: KeyInput[]): Promise<void>;
-    getDeviceInfo(): Promise<string>;
-}
--- dist/network/network.d.ts
+++
@@ -1,3 +0,0 @@
-export declare let RokuAddress: string;
-export declare const GetRokuAddress: () => Promise<string>;
-export declare const CheckRokuAddress: () => Promise<boolean>;
